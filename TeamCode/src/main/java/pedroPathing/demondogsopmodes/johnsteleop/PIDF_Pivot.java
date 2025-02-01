3f1cc2ba7920bc769ae2861f3f6ae9d424ad96b5
myleshill1 <myleshill@gmail.com>
2025-02-01 17:44:44 -0500

    a

--- TeamCode/src/main/java/pedroPathing/demondogsopmodes/johnsteleop/PIDF_Pivot.java
+++ TeamCode/src/main/java/pedroPathing/demondogsopmodes/johnsteleop/PIDF_Pivot.java
@@ -25,27 +25,20 @@ public class PIDF_Pivot extends OpMode {
 
     public static int target = 0;
 
-    private final double ticks_in_degree = 5281.1 / 180;
-    private DcMotorEx Lpivot;
+    private final double ticks_in_degree = 1425.1 / 180;
+    private DcMotorEx pivot;
 
-    private DcMotorEx Rpivot;
 
     @Override
     public void init() {
 
         controller = new PIDController(p, i, d);
-        telemetry = new MultipleTelemetry( telemetry , FtcDashboard.getInstance().getTelemetry());
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        Lpivot = hardwareMap.get(DcMotorEx.class, "Lpivot");
-
-        Rpivot = hardwareMap.get(DcMotorEx.class, "Rpivot");
-
-        Rpivot.setDirection(DcMotorSimple.Direction.REVERSE);
+        pivot = hardwareMap.get(DcMotorEx.class, "pivot");
 
 
-
-//        Rpivot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        Lpivot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        pivot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
 
     }
@@ -56,31 +49,26 @@ public class PIDF_Pivot extends OpMode {
 
         controller.setPID(p, i, d);
 
-        int LpivotPos = Lpivot.getCurrentPosition();
-        int RpivotPos = Rpivot.getCurrentPosition();
+        int pivotPos = pivot.getCurrentPosition();
 
-        double pid = controller.calculate(LpivotPos, target);
+
+        double pid = controller.calculate(pivotPos, target);
 
         double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
 
         double power = pid + ff;
 
 
+        pivot.setPower(power);
 
-        Lpivot.setPower(power);
-        Rpivot.setPower(power);
 
-        telemetry.addData("LpivotPos: ", LpivotPos);
-        telemetry.addData("Target: ", target);
-
-        telemetry.addData("RpivotPos: ", RpivotPos);
+        telemetry.addData("pivotPos: ", pivotPos);
         telemetry.addData("Target: ", target);
 
         telemetry.update();
 
     }
-
-
-
-
 }
+
+
+
